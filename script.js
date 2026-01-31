3fb9ada2a39daba2a543029c4387da86e03d4d0f
Blessed iboi <iboiblessed1@gmail.com>
2026-01-31 15:07:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,29 @@
-let version = "kjv";
-
-function changeVersion(v){
- version = v;
-}
-
-async function loadVerse(book, chapter, verse){
+async function loadRange(book, chapter, start, end){
 
  let file =
- version === "kjv"
- ? "bible_kjv.json"
- : "bible_niv.json";
-
- try{
+ version === "kjv" ? "bible_kjv.json" : "bible_niv.json";
 
  let data = await fetch(file);
  let bible = await data.json();
 
- let text =
- bible[book]?.[chapter]?.[verse];
+ let verses = [];
 
- if(text){
-   showOverlay(book, chapter, verse, text);
- }else{
-   showOverlay(book, chapter, verse,
-   "Verse not found in database");
+ for(let v = Number(start); v <= Number(end); v++){
+
+   let t = bible[book]?.[chapter]?.[v];
+
+   if(t){
+     verses.push(v + ". " + t);
+   }
  }
 
- }catch(e){
-   alert("Bible file missing");
- }
+ showOverlay(book, chapter, start, end, verses.join(" "));
 }
 
-function showOverlay(book, chapter, verse, text){
+function showOverlay(book, chapter, s, e, text){
 
- let box =
- document.getElementById("overlay");
-
- box.innerHTML = `
- <b>${book.toUpperCase()} ${chapter}:${verse}</b>
- <br>${text}
+ document.getElementById("overlay").innerHTML = `
+ <b>${book.toUpperCase()} ${chapter}:${s}-${e}</b><br>
+ ${text}
  `;
 }
-  
